ccda768ddbc236e4988ecc1529efc841bdeb7876
lolcabb <akarawat.wang@gmail.com>
2025-07-23 04:08:59 +0700

    refactor: move customers and container routes to controllers

---
+++ controllers/customerController.js
@@ -0,0 +1,18 @@
+module.exports = {
+  searchCustomers: (req, res) => res.status(200).end(),
+  getRouteAnalytics: (req, res) => res.status(200).end(),
+  getDeliveryRoutes: (req, res) => res.status(200).end(),
+  createDeliveryRoute: (req, res) => res.status(201).end(),
+  updateDeliveryRoute: (req, res) => res.status(200).end(),
+  deleteDeliveryRoute: (req, res) => res.status(200).end(),
+  getCreditSales: (req, res) => res.status(200).end(),
+  createCreditPayment: (req, res) => res.status(201).end(),
+  getCreditPayments: (req, res) => res.status(200).end(),
+  voidCreditPayment: (req, res) => res.status(200).end(),
+  editCreditPayment: (req, res) => res.status(200).end(),
+  createCustomer: (req, res) => res.status(201).end(),
+  listCustomers: (req, res) => res.status(200).end(),
+  getCustomer: (req, res) => res.status(200).end(),
+  updateCustomer: (req, res) => res.status(200).end(),
+  deleteCustomer: (req, res) => res.status(200).end()
+};
