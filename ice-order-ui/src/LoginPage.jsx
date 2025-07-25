36613317ed29f662bc7c654fe43fb80a7a31f2b8
lolcabb <akarawat.wang@gmail.com>
2025-07-26 05:58:00 +0700

    minor thai translate

--- ice-order-ui/src/LoginPage.jsx
+++ ice-order-ui/src/LoginPage.jsx
@@ -82,7 +82,7 @@ function LoginPage({ onLoginSuccess }) {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+            ลงชื่อเข้าสู่ระบบ
           </h2>
         </div>
         
