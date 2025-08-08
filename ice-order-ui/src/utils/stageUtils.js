30e9feb6068c72c13fff4f4a924efcb6029c4da7
lolcabb <akarawat.wang@gmail.com>
2025-08-08 09:45:19 +0700

    fix export

--- ice-order-ui/src/utils/stageUtils.js
+++ ice-order-ui/src/utils/stageUtils.js
@@ -1,7 +1,5 @@
-const isROStage = (stage) => {
+export const isROStage = (stage) => {
   if (!stage) return false;
   const name = stage.stage_name?.toLowerCase() || '';
   return name.includes('reverse osmosis') || name === 'ro' || stage.stage_id === 5;
 };
-
-module.exports = { isROStage };
